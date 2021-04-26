c4878535d4e65d9380505df8ccfe4b10d8763a4d
Jeanne d'Arc <njoanc@gmail.com>
2021-04-26 12:28:01 +0200

    json

--- myApp.js
+++ myApp.js
@@ -12,4 +12,10 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/json", function (req, res) {
+  res.json({
+    message: "Hello json",
+  });
+});
+
 module.exports = app;
