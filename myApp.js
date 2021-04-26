c107e1a5288a9a5d2cbf2675808085e8293b375b
Jeanne d'Arc <njoanc@gmail.com>
2021-04-26 10:55:54 +0200

    adding express server

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
-var express = require("express");
-var app = express();
+// var express = require("express");
+// var app = express();
 
 console.log("Hello World");
 
-module.exports = app;
+// module.exports = app;
