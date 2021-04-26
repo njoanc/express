b67c084e85c3e30a55d688cb8980d628f00f0883
Jeanne d'Arc <njoanc@gmail.com>
2021-04-26 11:35:43 +0200

    adding public folder

--- myApp.js
+++ myApp.js
@@ -5,6 +5,8 @@ var app = express();
 //   res.send("Hello Express");
 // });
 app.use(express.static(__dirname + "/public"));
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
