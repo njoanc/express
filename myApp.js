7a05287fed2a827b30d8a068bb6ced0e002360f3
Jeanne d'Arc <njoanc@gmail.com>
2021-04-26 11:07:17 +0200

    rendering a html file

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello Express");
-});
+// app.get("/", function (req, res) {
+//   res.send("Hello Express");
+// });
+
+app.get("/", __dirname + views / index.html);
 
 module.exports = app;
